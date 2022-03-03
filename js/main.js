3af130845bd9a8e4bb331dde06bd04a469b4303c
Rashmi Raj <rashmiraj7877@gmail.com>
2022-03-03 18:28:07 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const getResource = async () => {
+        let res=await fetch('https://api.adviceslip.com/advice');
+        if(!res.ok) throw new Error (`Could not fetch ${url}, status ${res.status}`);
+        return await res.json();
+    }
+    const title=document.querySelector('.title');
+    const quote=document.querySelector('.quote');
+    const btn=document.querySelector('.dice');
+    function setData(data) {
+        console.log(data);
+        title.textContent='advice #'+data.id;
+        quote.textContent=data.advice;
+    }
+    btn.addEventListener('click', () => {
+        getResource().then(data => setData(data.slip));
+    })
+})
